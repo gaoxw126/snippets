0d667872b8330dda1e847280abc2e9802a7c2837
吴涛 <wutao163.com@163.com>
2016-02-06 10:26:09 +0800

    add copyright headers

---
+++ copyright-header/GPLv3.h
@@ -0,0 +1,21 @@
+#if ($HEADER_COMMENTS)
+#set($COPYRIGHT_YEAR = ${YEAR })
+#set($COPYRIGHT_HOLDER = "Wu Tao")
+/**
+ * Copyright (C) $YEAR, ${COPYRIGHT_HOLDER}#if
+ *(!$COPYRIGHT_HOLDER.endsWith(".")).#end All rights reserved.
+ *
+ * ${PROJECT_NAME} is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ${PROJECT_NAME} is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#end
---
+++ copyright-header/README.md
@@ -0,0 +1,7 @@
+### copyright-header 
+
+*****copyright-header** provides [velocity](https://en.wikipedia.org/wiki/Apache_Velocity)
+templates used to generate copyright notices which can be placed in header of your codes.
+
+Velocity templates are supported by Jetbraints' IDEs. See manuals offered by Jetbraints
+about how to add velocity templates if you are using their products.
