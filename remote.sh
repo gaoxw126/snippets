a3180b344eead3b4dbd5f3a3258751c97f3b6c9f
Wu Tao <wutao163.com@163.com>
2016-10-31 11:17:05 +0800

    Create remote.sh

---
+++ remote.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# 更新源
+curl http://172.22.0.16:8082/install_repo.bash | bash
+
+sudo apt update
+sudo apt install zsh git golang build-essential cmake libboost-all-dev
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# 安装 fio
+sudo apt install fio
+
+# 安装 spf13-vim
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+
+# 安装 clang3.8
+sudo apt-get install clang-3.8 llvm-3.8 libclang-3.8-dev
+
+# 安装 tidb 必要依赖
+git clone https://github.com/pingcap/docs.git tidb-docs
+cd tidb-docs
+sudo ./scripts/check_requirement.sh
+
+# 进行 tidb 的编译
+cd ~
+mkdir tidb-build && cd tidb-build
+sh ~/tidb-docs/scripts/build.sh
+
+# 进行 folly 的编译
+cd ~
+git clone httpvs://github.com/facebook/folly.git
+cd folly/folly
+git checkout v0.57.0 # 其他版本会出现错误
+
+sudo apt install \
+    g++ \
+    automake \
+    autoconf \
+    autoconf-archive \
+    libtool \
+    libboost-all-dev \
+    libevent-dev \
+    libdouble-conversion-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libsnappy-dev \
+    make \
+    zlib1g-dev \
+    binutils-dev \
+    libjemalloc-dev \
+    libssl-devc
+
+autoreconf -ivf
+./configure
+make
+make check
+sudo make install
+mkdir -p /mnt/vdb && mount /dev/vdb1 /mnt/vdb
+
+# 安装 vim with python，针对 ubuntu 14.04
+sudo add-apt-repository ppa:pi-rho/dev
+sudo apt-get update
+sudo apt install vim
+
+vim +PlugInstall +qall
